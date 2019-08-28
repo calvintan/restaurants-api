5aaaf642c048372c729dde1ec817abce44202f49
Calvin Tan <hello@calvin-tan.com>
2019-08-28 11:18:13 +0800

    Format the json structure

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
   def index
-    @restaurant = Restaurant.all
-    # render json: @restaurant
+    @restaurants = Restaurant.all
   end
 end
