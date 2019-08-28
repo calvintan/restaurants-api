08c0442a059b9ca08a63809239236fae6309e7a4
Calvin Tan <hello@calvin-tan.com>
2019-08-28 11:59:20 +0800

    Add update route and controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
-  before_action :find_restaurant, only: [:show]
+  skip_before_action :verify_authenticity_token
+  before_action :find_restaurant, only: [:show, :update]
 
   def index
     @restaurants = Restaurant.all
@@ -8,9 +9,26 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   def show
   end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
 
   def find_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :image, :description)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
