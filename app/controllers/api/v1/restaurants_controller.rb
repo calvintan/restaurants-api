c6164a4e33355bdc742e6fcc2cf6b1a6a0cdb5a4
Calvin Tan <hello@calvin-tan.com>
2019-08-28 11:32:35 +0800

    Add a show route to display data for a single restaurant

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  before_action :find_restaurant, only: [:show]
+
   def index
     @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
